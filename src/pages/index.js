5c0abf3043b1998002080b890bf532a0c8b54ef3
Benjamin Gröner <benjamin.groener@seedbox-ventures.com>
2021-08-11 16:40:13 +0200

    Revert "[TEST] Another preview test"
    This reverts commit b61b2e5ea7a4bb77779b65e817c86bd643b40364.

--- src/pages/index.js
+++ src/pages/index.js
@@ -68,7 +68,7 @@ export const query = graphql`
 
 export default withPrismicPreview(HomeTemplate, [
   {
-    repositoryName: process.env.PRISMIC_REPO_NAME,
+    repositoryName: 'pageport-playground',
     accessToken: 'MC5ZUlBXVWhJQUFDNEFDdm5K.bCZ-77-977-9G3fvv70dEO-_vVHvv73vv73vv71bHmZp77-977-977-9Be-_ve-_vUXvv70A77-977-9Y2E',
     linkResolver,
   },
