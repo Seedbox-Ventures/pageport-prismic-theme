416ac117c117e41b067064e92a952d924343f5f9
Benjamin Gröner <benjamin.groener@seedbox-ventures.com>
2021-08-12 16:44:17 +0200

    [TASK] Update Prismic config

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -1,3 +1,7 @@
-{
-  "apiEndpoint": "https://pageport-playground.prismic.io/api/v2"
-}
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+module.exports = {
+  apiEndpoint: `https://${process.env.PRISMIC_REPO_NAME}.prismic.io/api/v2`,
+};
