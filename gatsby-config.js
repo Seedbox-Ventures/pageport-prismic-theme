d439c89dbcd66d8d96792aa692dd326bf3b3dafc
Benjamin Gröner <benjamin.groener@seedbox-ventures.com>
2021-08-15 12:39:13 +0200

    [TASK] Make pageport configurable with ts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,53 +1,8 @@
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
-
 module.exports = {
-  siteMetadata: {
-    title: 'Gatsby + Prismic Tutorial',
-    description: 'Learn how to integrate Prismic into your Gatsby project.',
-  },
   plugins: [
     {
-      resolve: 'gatsby-plugin-prismic-previews',
-      options: {
-        repositoryName: process.env.PRISMIC_REPO_NAME,
-        accessToken: process.env.PRISMIC_API_KEY,
-      },
-    },
-    {
-      resolve: 'gatsby-source-prismic',
-      options: {
-        repositoryName: process.env.PRISMIC_REPO_NAME,
-        accessToken: process.env.PRISMIC_API_KEY,
-        linkResolver: require('./src/utils/LinkResolver').linkResolver,
-        schemas: {
-          page_dynamic: require('./custom_types/page_dynamic.json'),
-          page_imprint: require('./custom_types/page_imprint.json'),
-          page_legal: require('./custom_types/page_legal.json'),
-          theme: require('./custom_types/theme.json'),
-        },
-      },
-    },
-    'gatsby-plugin-image',
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        icon: 'src/images/favicon.png',
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-google-fonts',
-      options: {
-        fonts: [`Lato\:400,400,700,700i,900`, `Amiri\:400,400,700,700i`],
+      resolve: 'gatsby-plugin-ts-config', options: {
+        configDir: `${process.cwd()}/gatsby-config`,
       },
     },
   ],
