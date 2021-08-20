5dc47d3905c4f9e9cca2f56ba75a436eecd7ec74
Benjamin Gröner <benjamin.groener@seedbox-ventures.com>
2021-08-20 14:02:59 +0200

    [FIX] Fix value merging

--- src/theme/ThemeWrapper.tsx
+++ src/theme/ThemeWrapper.tsx
@@ -19,7 +19,8 @@ export const defaultValues: ThemeValues = Theme.mapThemingData(themeConfig.defau
 export const ThemeWrapper: React.FC<ThemeWrapperProps> = ({ themeValues, children, isRootTheme = false }) => {
 
   const prismicThemeValues: Partial<ThemeValues> = themeValues ? Theme.mapThemingData(themeValues) : {}
-  const mergedThemeValues: ThemeValues = _.merge({ ...defaultValues, ...prismicThemeValues })
+  const mergedThemeValues: ThemeValues = _.merge(defaultValues, prismicThemeValues)
+
 
   const theme = new Theme(mergedThemeValues)
 
