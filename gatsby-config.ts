da817f2947702c1fc97c5a8e92a2cd978f42739d
Benjamin Gröner <benjamin.groener@seedbox-ventures.com>
2021-08-20 08:57:18 +0200

    [FIX] Readd the realse ID to sources config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -21,7 +21,7 @@ export default {
       options: {
         repositoryName: process.env.PRISMIC_REPO_NAME,
         accessToken: process.env.PRISMIC_API_KEY,
-        releaseID: process.env.GATSBY_PRISMIC_RELEASE_ID,
+        releaseID: process.env.PRISMIC_RELEASE_ID,
         linkResolver: require('./src/utils/LinkResolver').linkResolver,
         schemas: {
           page_dynamic: require('./custom_types/page_dynamic.json'),
