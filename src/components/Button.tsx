be23e0cc2dd082ba2d63147836219c030627c816
fafn <ff@uf-web.de>
2021-08-20 11:27:55 +0200

    implemented button hover effects

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react'
 import styled from 'styled-components'
-import { ThemeButtonType, ThemeTextType } from '../theme/types'
+import { ThemeButtonType, ThemeTextType, ThemeButtonHoverEffectType } from '../theme/types'
 import { Link } from 'gatsby'
 import { linkResolver } from '../utils/LinkResolver'
 import { PrismicLinkType, PrismicLinkTypeEnum } from '../types/PrismicLink'
+import tinycolor from 'tinycolor2'
 
 export interface ButtonProps {
   text: string
@@ -25,26 +26,49 @@ const StyledButton = styled.div<{ buttonType: ThemeButtonType }>(({
       width: theme.values.buttonBorderWidth,
       color: buttonColorValue,
     },
-    boxShadow: theme.values.buttonBoxShadow || 'none',
+    boxShadow: theme.values.buttonBoxShadow,
     fontFamily: theme.getFontFamily((theme.getType(ThemeTextType.Button) || theme.getStandardType()).fontFamily),
     bgColor: !(buttonConfig.fillBackground) ? 'transparent' : buttonColorValue,
     textColor: !(buttonConfig.fillBackground) ? buttonColorValue : theme.getTextColorValueByBackgroundValue(buttonColorValue),
   }
 
+  const lightenDarken = (colorValue: `#${string}`, amount = 10): `${string}` => `${(tinycolor(colorValue).isDark() ? tinycolor(colorValue).brighten(amount) : tinycolor(colorValue).darken(amount)).toHexString()}`;
+
+  const buttonHoverEffects: Record<ThemeButtonHoverEffectType, any> = {
+    [ThemeButtonHoverEffectType.None]: `
+      
+    `,
+    [ThemeButtonHoverEffectType.ChangeBackground]: `
+      background-color: ${!(buttonConfig.fillBackground) ? buttonColorValue : 'transparent'};
+      color: ${!(buttonConfig.fillBackground) ? theme.getTextColorValueByBackgroundValue(buttonColorValue) : buttonColorValue};
+    `,
+    [ThemeButtonHoverEffectType.ChangeBoxShadow]: `
+      /* TODO: implement ChangeBoxShadow effect */
+    `,
+    [ThemeButtonHoverEffectType.DarkenLighten]: `
+      background-color: ${lightenDarken(buttonColorValue)};
+      border-color: ${lightenDarken(buttonColorValue)};
+    `
+  }
+
   return `
-  display: inline-block;
-  margin: 0;
-  padding: ${values.padding};
-  border: ${values.border.width} solid ${values.border.color};
-  border-radius: ${values.borderRadius};
-  background-color: ${values.bgColor};
-  box-sizing: border-box;
-  text-decoration: none;
-  font-family: ${values.fontFamily};
-  color: ${values.textColor};
-  text-align: center;
-  transition: all 0.2s;
-  line-height: 1;
+    display: inline-block;
+    margin: 0;
+    padding: ${values.padding};
+    border: ${values.border.width} solid ${values.border.color};
+    border-radius: ${values.borderRadius};
+    background-color: ${values.bgColor};
+    box-sizing: border-box;
+    text-decoration: none;
+    font-family: ${values.fontFamily};
+    color: ${values.textColor};
+    text-align: center;
+    transition: all 0.2s;
+    line-height: 1;
+    
+    &:hover, &:focus {
+      ${buttonHoverEffects[buttonConfig.hoverEffect]}
+    }
   `
 })
 
