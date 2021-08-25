3cce7b086af10f0bbb50cc0b32657cb8d269c52b
Benjamin Gröner <benjamin.groener@seedbox-ventures.com>
2021-08-25 11:34:22 +0200

    [FIX] Fix Linkresolver

--- src/utils/LinkResolver.tsx
+++ src/utils/LinkResolver.tsx
@@ -1,9 +1,14 @@
 import { PrismicDocument } from '@prismicio/types'
 import { LinkResolver } from 'prismic-reactjs'
 
+const staticMapping: Record<string, string> = {
+  index: '/',
+}
+
 export const linkResolver: LinkResolver = (doc: PrismicDocument) => {
   if (doc.type === 'page_dynamic') {
-    return `/${doc.uid}`
+
+    return staticMapping[doc.uid!] ? staticMapping[doc.uid!] : `/${doc.uid}`
   }
 
   return '/'
