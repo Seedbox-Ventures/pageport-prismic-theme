8aa31eeb9987f573f09858526ca6517e0be16cb7
Benjamin Gröner <benjamin.groener@seedbox-ventures.com>
2021-09-29 17:36:58 +0200

    [TASK] Some cookie banner ui progress

--- src/modules/dataProtection/ConsentBanner.tsx
+++ src/modules/dataProtection/ConsentBanner.tsx
@@ -1,28 +1,29 @@
 import React from 'react'
-import _ from 'lodash'
-import { Overlay } from '../overlay/Overlay'
 import styled from 'styled-components'
+import { Overlay } from '../overlay/Overlay'
 import { Section } from '../page'
-import { ThemeButtonType, ThemeColorType } from '../../theme'
 import { Button } from '../basic/Button'
 import { ConsentBannerState, DataSink } from './types'
 import { useAppDispatch, useAppSelector } from '../../state/hooks'
 import { acceptAll, selectBannerState, selectDataSinks } from './dataProtectionSlice'
+import { ThemeTextType } from '../../theme'
+import { Link } from '../basic/Link'
 
 interface ConsentBannerProps {}
 
-const StyledConsentBanner = styled.div`
-  position: absolute;
-  bottom: 0;
-  width: 100vw;
-  height: auto;
-`
+const StyledConsentBanner = styled.div<{ textType?: ThemeTextType }>(({ textType, theme }) => {
+  return `
+    position: absolute;
+    bottom: 0;
+    width: 100vw;
+    height: auto;
+    ${textType ? theme.renderTextTypeCss(textType) : ''} 
+  `
+})
 
-const StyledLink = styled.a`
-  cursor: pointer;
-`
 export const ConsentBanner: React.FC<ConsentBannerProps> = () => {
-  const { isOpen }: ConsentBannerState = useAppSelector(selectBannerState)
+  const { isOpen, backgroundColor, buttonType, explanationText, textType }: ConsentBannerState =
+    useAppSelector(selectBannerState)
   const dataSinks: Array<DataSink> = useAppSelector(selectDataSinks)
   const dispatch = useAppDispatch()
   const acceptAllFunc = () => {
@@ -33,20 +34,26 @@ export const ConsentBanner: React.FC<ConsentBannerProps> = () => {
     <>
       {!!dataSinks?.length && (
         <Overlay isOpen={isOpen} open={() => void 0} close={() => void 0} backdropBackground={'rgba(0,0,0,0.65)'}>
-          <StyledConsentBanner className="consentBanner">
+          <StyledConsentBanner className="consentBanner" textType={textType}>
             <Section
+              className="consentBanner__container"
               as={'div'}
-              backgroundColor={ThemeColorType.Primary}
-              customContainerStyle={{ 'flex-direction': 'row' }}
+              backgroundColor={backgroundColor}
+              customContainerStyle={{
+                display: 'grid',
+                'column-gap': '1rem',
+                'row-gap': '1rem',
+              }}
             >
-              <div>
-                Wir nutzen Cookies auf unserer Website. Einige von ihnen sind essenziell, während andere uns helfen,
-                diese Website und Ihre Erfahrung zu verbessern.
-                <StyledLink>Nur essenzielle Cookies akzeptieren</StyledLink>
-                <StyledLink>Individuelle Datenschutzeinstellungen</StyledLink>
+              <div className="consentBanner_explanation">
+                <p>{explanationText}</p>
               </div>
-              <div>
-                <Button type={ThemeButtonType.Secondary} onClick={acceptAllFunc}>
+              <div className="consentBanner_links">
+                <Link>Nur essenzielle Cookies akzeptieren</Link>
+                <Link>Individuelle Datenschutzeinstellungen</Link>
+              </div>
+              <div className="consentBanner__col-2">
+                <Button type={buttonType} onClick={acceptAllFunc}>
                   Alle akzeptieren
                 </Button>
               </div>
