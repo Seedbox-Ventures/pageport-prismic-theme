f3c8115b7a689d034af24fe35279193765fef137
Benjamin Gröner <benjamin.groener@seedbox-ventures.com>
2021-09-30 16:48:59 +0200

    [TASK] Extend the global styling

--- src/theme/GlobalStyle.tsx
+++ src/theme/GlobalStyle.tsx
@@ -5,34 +5,40 @@ import { createGlobalStyle, ThemeContext } from 'styled-components'
 import { ThemeTextType } from './types'
 import { Helmet } from 'react-helmet'
 
-const StyleComponent = createGlobalStyle<{}>(({ theme }) => {
-  return `
+const StyleComponent = createGlobalStyle<{}>(
+  ({ theme }) => `
   body {
     ${theme.renderTextTypeCss(ThemeTextType.StandardText)}
   }
-`
-})
+  
+  h1 {
+    ${theme.renderTextTypeCss(ThemeTextType.PageTitle)}
+  }
+  
+  h2 {
+    ${theme.renderTextTypeCss(ThemeTextType.SectionTitle)}
+  }
+  
+  h3 {
+    ${theme.renderTextTypeCss(ThemeTextType.SectionTitle)}
+  }
+`,
+)
 
 export const GlobalStyle: React.FC = () => {
   const themeContext = useContext(ThemeContext)
   const fontLinks: ReactNodeArray = _.map(
-    _.omitBy(
-      [themeContext.props.primaryFontImportLink, themeContext.props.secondaryFontImportLink], _.isEmpty,
-    ),
+    _.omitBy([themeContext.props.primaryFontImportLink, themeContext.props.secondaryFontImportLink], _.isEmpty),
     (link, i) => {
-      return <link key={`font-import${i}`} rel='stylesheet' href={link.url} />
+      return <link key={`font-import${i}`} rel="stylesheet" href={link.url} />
     },
   )
 
-  return <>
-    {
-      fontLinks.length
-      && <Helmet>
-        {fontLinks}
-      </Helmet>
-    }
+  return (
+    <>
+      {fontLinks.length && <Helmet>{fontLinks}</Helmet>}
 
-    <StyleComponent />
-  </>
+      <StyleComponent />
+    </>
+  )
 }
-
