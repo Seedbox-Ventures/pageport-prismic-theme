c5748f442eb7e6d34cecbcdb35d18f70d247e49d
Benjamin Gröner <benjamin.groener@seedbox-ventures.com>
2021-10-12 13:49:11 +0200

    [TASK] Remove footer stuff

--- src/modules/page/Footer.tsx
+++ src/modules/page/Footer.tsx
@@ -2,12 +2,6 @@ import * as React from 'react'
 
 const Footer: React.FC = () => (
   <footer>
-    <p>
-      Proudly published with{' '}
-      <a href="https://prismic.io" target="_blank" rel="noopener noreferrer">
-        Prismic
-      </a>
-    </p>
   </footer>
 )
 
